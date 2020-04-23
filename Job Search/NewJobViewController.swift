d5fa6ac2cbad9d26385f710407ec8c946185d70b
Jasjeev <jasjeev4@gmail.com>
2020-04-23 21:21:59 +0530

    Add Readme

--- Job Search/NewJobViewController.swift
+++ Job Search/NewJobViewController.swift
@@ -33,11 +33,13 @@ class NewJobViewController: UIViewController {
         }
         else {
             // Load image url
+            self.showSpinner(onView: self.view)
             Client.loadLogo(company: jobTitle.text!, completion: handleLogoResponse(photoURL:error:))
         }
     }
     
     func handleLogoResponse(photoURL: String, error: Error?) {
+        self.removeSpinner()
     
         if (error != nil) {
             print("There was an error")
@@ -75,5 +77,33 @@ class NewJobViewController: UIViewController {
         let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         self.present(alertVC, animated: true, completion: nil)
+    }
+}
+
+//Credit http://brainwashinc.com/2017/07/21/loading-activity-indicator-ios-swift/
+
+var vSpinner : UIView?
+
+extension UIViewController {
+    func showSpinner(onView : UIView) {
+        let spinnerView = UIView.init(frame: onView.bounds)
+        spinnerView.backgroundColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
+        let ai = UIActivityIndicatorView.init(style: .whiteLarge)
+        ai.startAnimating()
+        ai.center = spinnerView.center
+        
+        DispatchQueue.main.async {
+            spinnerView.addSubview(ai)
+            onView.addSubview(spinnerView)
+        }
+        
+        vSpinner = spinnerView
+    }
+    
+    func removeSpinner() {
+        DispatchQueue.main.async {
+            vSpinner?.removeFromSuperview()
+            vSpinner = nil
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-Job Search
+# Job Tracker
+
+
+
+This app allows you to fill in a text field with a company's name and a job position. 
+
+This then adds the job posting to your Job Tracker
+
+On the Job tracker page you can log your progress as you apply and go an offer to a position.
+
+There's also a convenient sort option to sort jobs you've added by recent or oldest.
+
+The following is what a populated tracker can look like.
+
+
+
+![Screen Shot 2020-04-23 at 9.17.23 PM.png](/var/folders/wj/hy0xr8rs5q5493lq60fwfqh80000gn/T/TemporaryItems/(A%20Document%20Being%20Saved%20By%20screencaptureui%202)/Screen%20Shot%202020-04-23%20at%209.17.23%20PM.png)
+
+
